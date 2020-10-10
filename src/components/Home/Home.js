fc9c4a71242d24635c9bbc643ed66146ea051778
Mohamad Dabboussi <dabboussi99@gmail.com>
2020-10-11 00:27:43 +0300

    Add banner

---
+++ src/components/Home/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <img
+        className="home_image"
+        src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_Currency_v2_en_US_2x._CB428993290_.jpg"
+        // src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_45M_v2_1x._CB432458380_.jpg"
+        alt=""
+      />
+    </div>
+  );
+}
+
+export default Home;
