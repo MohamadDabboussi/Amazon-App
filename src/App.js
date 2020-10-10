a58f0eac66631f9b63decbb739802545c4de7b8d
Mohamad Dabboussi <dabboussi99@gmail.com>
2020-10-10 12:03:04 +0300

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h2>Hello from amazon app</h2>
-      <h3>Mohamad Dabboussi </h3>
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/checkout">
+            <h1>CHECKOUT</h1>
+          </Route>
+          <Route path="/login">
+            <h1>Login</h1>
+          </Route>
+          <Route path="/">
+            <h1>Home</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
